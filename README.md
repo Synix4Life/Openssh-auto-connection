b3183681dcb97b687c5859e43bcb8a615041e34c
Synix4Life <nt.lang2002@gmail.com>
2024-04-20 22:11:57 +0200

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# SSH- Server connection automation
+
+![Version](https://img.shields.io/badge/Version-1.0.0-black?style=for-the-badge)
+![Language](https://img.shields.io/badge/Bash_Script-black?style=for-the-badge&logo=gnu-bash&logoColor=white)
+![License](https://img.shields.io/badge/License-aa-black?style=for-the-badge)
+
+---
+
+## Explanation
+
+This is a shell- script for linux, which was created to automate the process of connecting to an SSH- Server.
+
+The script was initially created to connect to the ARM- SSH- Server of the TU Darmstadt, but can be modified and used with other SSH- Servers as well.
+
+The script is designed to use a public and a private key, generated with putty, to authenticate yourself. 
+
+---
+
+If you have any questions or ideas to improve the script, feel free to change them or contact me about it
---
+++ init_ARM_openssh_connection.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo -e "\nExpected one argument"
+	echo -e "\nUsage: ./openssh.sh username!\n"
+	exit 1
+fi
+
+username=$1
+
+ssh -V
+
+echo "Already initalized the connection?[y/n]"
+
+read already
+
+if [ "$already" = "n" ]; then
+
+	# Checking if openssh-client is already installed
+	# If not it installs itself
+	if ! dpkg -s openssh-client > /dev/null 2>&1; then
+	    echo "openssh-client is not installed. Installing..."
+	    sudo apt-get install openssh-client
+	else
+	    echo "openssh-client is already installed."
+	fi
+
+	# Question about privateKey
+	echo "Do you already have a privateKey.ppk?[y/n]"
+	read choice1
+	if [ "choice1" = "n" ]; then
+		echo "Please install putty from the official website, then continue"
+		read pass0
+
+		# Checking if wine is already installed
+		# If not it install itself
+		if ! dpkg -s wine > /dev/null 2>&1; then
+            echo "wine is not installed. Installing..."
+       	    sudo apt-get install wine
+        fi
+
+		# Executing putty to generate keys
+		wine putty.exe
+		wine puttygen
+	fi
+
+	# Checking if putty-tools is already installed
+        # If not it installs itself
+        if ! dpkg -s putty-tools > /dev/null 2>&1; then
+            echo "putty-tools is not installed. Installing..."
+            sudo apt-get install putty-tools
+        else
+            echo "putty-tools is already installed."
+        fi
+
+	# Checking if the .ppk- file is in directory
+	# If not, exit with error
+	ppkFile=$(find . -name "*.ppk" -print -quit)
+	if [ -n "$ppkFile" ]; then
+		echo "Couldn't find *.ppk- File"
+		exit 1
+	fi
+
+	# Asking about the file- name
+	echo "Under which name shall the new Key- File be stored?"
+	read i
+	key="$i.pem"
+
+	# Creating a .pem- file from the existing .ppk- file
+	puttygen "$ppkFile" -O private-openssh -o "$key"
+	chmod 600 "$key"
+
+else
+
+	key=$(find . -name "*.pem" -print -quit)
+	if [ -n "$java_file" ]; then
+		echo "Couldn't find the *.pem key- file"
+	else
+		echo "key- file $key found, executing connection"
+	fi
+
+fi
+
+# You have to add the signature to the SSH- Server!
+# Remove the "___" and add tbe SSH- Server
+ssh -i "$key" "$1"@___
